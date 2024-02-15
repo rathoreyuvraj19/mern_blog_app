f6129c38b1708e6a8354a26cfc83c348cab8f64d
Yuvraj Singh Rathore <rathore.yuvraj19@gmail.com>
2024-02-15 09:22:19 +0530

    Routes setup and controllers setup test done

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,18 @@
 import express from "express";
 import { User } from "./db.js";
 import dotenv from "dotenv";
+import userRouter from "./routes/user.js";
+import cors from "cors";
 
 dotenv.config();
 
 const port = process.env.PORT || 3000;
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("hello");
-});
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/v1", userRouter);
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
