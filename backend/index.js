4815b2b54525a3b4423cb4a552b1185a188bd964
Yuvraj Singh Rathore <rathore.yuvraj19@gmail.com>
2024-02-15 15:50:39 +0530

    auth route and component setup

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,9 @@
 import express from "express";
 import { User } from "./db.js";
 import dotenv from "dotenv";
-import userRouter from "./routes/user.js";
+import userRouter from "./routes/user.route.js";
 import cors from "cors";
+import signupRouter from "./routes/auth.route.js";
 
 dotenv.config();
 
@@ -13,6 +14,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/v1", userRouter);
+app.use("/api/v1/signup", signupRouter);
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
